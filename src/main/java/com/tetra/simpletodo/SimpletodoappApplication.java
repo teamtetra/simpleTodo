5e6f9480c80c68ff53dcd8658978a193cbcb879f
saakethvelamakanni <velamakanni.ps@gmail.com>
2020-10-20 21:06:25 -0500

    first commit

---
+++ src/main/java/com/tetra/simpletodo/SimpletodoappApplication.java
@@ -0,0 +1,13 @@
+package com.tetra.simpletodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpletodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpletodoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tetra/simpletodo/SimpletodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tetra.simpletodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpletodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
